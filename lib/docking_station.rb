9184fb6abfd85df3b8c582abdfcacdec6a56822e
John Connolly <johnc1983@hotmail.co.uk>
2022-04-18 18:10:54 +0100

    issues on 17 initialization defaults

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,19 +1,31 @@
 require_relative 'bike'
 
 class DockingStation  # dockingstation class
+  DEFAULT_CAPACITY = 20 #DEFAULT_CAPACITY is set to 20 so i can replace 20 in the code with DEFAULT CAPACITY 
   def initialize
     @bikes = []
   end 
 
   def release_bike   # inside the release_bike method we are returning a new instance of bike
-    fail 'No bikes available' if @bikes.empty?  #fails unless there is bike to  release
+    fail 'No bikes available' if empty?  #fails unless there is bike to  release
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking station full' if @bikes.count >= 20 #fails if there is more than 20 bikes in docking station
+    fail 'Docking station full' if capacity  #fails if there is more than 20 bikes in docking station
     @bikes << bike
   end
 
-  attr_reader :bike
+  attr_reader :bike 
+  attr_reader :capacity
+
+  private
+
+  def capacity
+    @bikes.count >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @bikes.empty?
+  end 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -30,17 +30,21 @@ describe DockingStation do
 
   describe '#dock' do
     it 'raises error when full' do
-      20.times { subject.dock Bike.new}
+      DockingStation::DEFAULT_CAPACITY.times do   # :: we use as a namespace operator to access the DEFAULT_CAPACITY contant n the DockingStation class
+        subject.dock Bike.new
+      end
       expect {  subject.dock Bike.new }.to raise_error 'Docking station full'
     end 
     
     # it 'docks something' do
-    #   bike = Bike.new        FIND OUT WHY I DID THIS LOOK THROUGH WALKTHROUGH 
+    #   bike = Bike.new        FIND OUT WHY I DID THIS LOOK THROUGH WALKTHROUGH
     #   subject.dock(bike)
     #   expect(subject.bike ).to eq @bikes 
     # end
   end 
 end
+
+
 
 # it 'releases working bikes' do
 #   bike = subject.release_bike
